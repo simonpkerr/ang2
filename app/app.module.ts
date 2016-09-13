0f6ddbceb54ecaa233f278e0369b201ea18e1241
Simon Kerr <simon.kerr@oegen.co.uk>
2016-09-13 12:36:24 +0100

    added simple barrel

--- app/app.module.ts
+++ app/app.module.ts
@@ -4,8 +4,12 @@ import { HttpModule } from '@angular/http';
 import 'rxjs/Rx';
 
 import { AppComponent }  from './app.component';
-import { CustomerComponent } from './customer/customer.component';
-import { CustomersComponent } from './customer/customers.component';
+// import { CustomerComponent } from './customer/customer.component';
+// import { CustomersComponent } from './customer/customers.component';
+
+// use a barrel to grab any or all components from a module
+// looks for an index file to grab the components
+import { CustomerComponent, CustomerService, CustomersComponent } from './customer';
 
 @NgModule({
   imports: [ BrowserModule, HttpModule ],
---
+++ app/customer/index.ts
@@ -0,0 +1,6 @@
+// this is a barrel that can export selected components,services etc that can be
+// injected into other components
+// barrels can be nested within other barrels
+export * from './customer.service';
+export * from './customer.component';
+export * from './customers.component';
